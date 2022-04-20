5c2f14138481ea7e5faacdae3bfc8df3d5aadf06
Raj Dubey <com.mailuser@gmail.com>
2022-04-20 23:41:23 +0530

    first version

--- app/src/main/java/com/authoritydmc/carddo/MainActivity.kt
+++ app/src/main/java/com/authoritydmc/carddo/MainActivity.kt
@@ -2,10 +2,19 @@ package com.authoritydmc.carddo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.authoritydmc.carddo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+
+
+        binding.textView.setText("THis is custom coding");
+
+
     }
 }
