47ae9f25d1a4a1e418c67e34c983118e3c5bcc85
Raj Dubey <com.mailuser@gmail.com>
2022-05-06 17:21:26 +0530

    asset folder create

--- app/src/main/java/com/authoritydmc/carddo/activities/MainActivity.kt
+++ app/src/main/java/com/authoritydmc/carddo/activities/MainActivity.kt
@@ -2,12 +2,16 @@ package com.authoritydmc.carddo.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.JsonReader
+import android.util.Log
 import android.widget.Toast
 import com.authoritydmc.carddo.api.retrofitClient
 import com.authoritydmc.carddo.databinding.ActivityMainBinding
 import com.authoritydmc.carddo.models.UpdatePOKO
 import com.authoritydmc.carddo.utility.CardView
 import com.authoritydmc.carddo.utility.UTILS
+import com.google.gson.Gson
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -16,6 +20,13 @@ class MainActivity : AppCompatActivity() {
     private  lateinit var binding:ActivityMainBinding
     private lateinit var cardView:CardView
     private lateinit var cardView2:CardView
+
+    companion object{
+        var CURRENT_VERSION="";
+    }
+    init {
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
@@ -28,9 +39,24 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
-
+checkforUpdate()
     }
 
+    private fun checkforUpdate() {
+        retrofitClient.instance.checkUpdate().enqueue(object : Callback<UpdatePOKO?> {
+            override fun onResponse(call: Call<UpdatePOKO?>, response: Response<UpdatePOKO?>) =
+                Toast.makeText(applicationContext,response.body()?.downloadURL,Toast.LENGTH_LONG).show()
+
+
+
+            override fun onFailure(call: Call<UpdatePOKO?>, t: Throwable) {
+                Toast.makeText(applicationContext,"Failed to get JSON",Toast.LENGTH_LONG
+                ).show()
+                Log.d("RAJ", "onResponse: found response ${t.message}")
+
+            }
+        })
+    }
 
 
 }
--- app/src/main/java/com/authoritydmc/carddo/utility/UTILS.kt
+++ app/src/main/java/com/authoritydmc/carddo/utility/UTILS.kt
@@ -1,11 +1,35 @@
 package com.authoritydmc.carddo.utility
 
+import android.content.res.loader.AssetsProvider
+import android.system.Os.open
+import com.authoritydmc.carddo.models.UpdatePOKO
+import com.google.gson.Gson
+import java.io.InputStream
+import java.io.IOException
+import java.nio.channels.DatagramChannel.open
+import java.nio.charset.Charset
+import kotlin.coroutines.coroutineContext
+
 class UTILS {
     companion object
     {
       final  val UPDATE_CHECK_URL:String="https://authoritydmc.github.io/";
         val PLACEHOLDERAPI="https://jsonplaceholder.typicode.com/"
+        fun readJSONFromAsset(): String? {
+            var json: String? = null
+            try {
+              //  val  inputStream: InputStream = AssetsProvider
+                //json = inputStream.bufferedReader().use{it.readText()}
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+                return null
+            }
+            return json
+        }
 
+        fun parseJSON(): UpdatePOKO {
+            return Gson().fromJson(readJSONFromAsset(), UpdatePOKO::class.java)
+        }
     }
 
 }
