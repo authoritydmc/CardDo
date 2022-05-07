9462489350ce63b3198a1647dfdce62dae1a8e15
Raj Dubey <com.mailuser@gmail.com>
2022-05-08 00:13:04 +0530

    0.0.14 auth added

--- app/src/main/java/com/authoritydmc/cardKart/activities/Splash.kt
+++ app/src/main/java/com/authoritydmc/cardKart/activities/Splash.kt
@@ -1,12 +1,19 @@
 package com.authoritydmc.cardKart.activities
 
 import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+
 import android.util.Log
+import android.widget.Button
 import android.widget.Toast
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import com.authoritydmc.cardKart.R
 
 import com.authoritydmc.cardKart.api.retrofitClient
@@ -16,9 +23,13 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.lang.Exception
+import java.util.concurrent.Executor
 
 class Splash : AppCompatActivity() {
 
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
 
     companion object{
         lateinit var CURRENT_VERSION:String;
@@ -30,6 +41,7 @@ class Splash : AppCompatActivity() {
         val json= UTILS.parseJSON(this);
 
         CURRENT_VERSION=json.version;
+        checkforauth()
         checkforUpdate()
 
 
@@ -48,6 +60,8 @@ class Splash : AppCompatActivity() {
     private fun routetoMain() {
 
 
+        checkforauth()
+
             Handler(Looper.getMainLooper()).postDelayed(Runnable {
                 val intent:Intent=Intent(this,MainActivity::class.java).apply {
                     addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
@@ -56,6 +70,52 @@ class Splash : AppCompatActivity() {
                 finish()
             },1000 )
         }
+
+    private fun checkforauth() {
+        executor=ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int,
+                                                   errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(applicationContext,
+                        "Authentication error: $errString", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onAuthenticationSucceeded(
+                    result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    Toast.makeText(applicationContext,
+                        "Authentication succeeded!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(applicationContext, "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setAllowedAuthenticators(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
+            .build()
+
+        // Prompt appears when user clicks "Log in".
+        // Consider integrating with the keystore to unlock cryptographic operations,
+        // if needed by your app.
+//        val biometricLoginButton =
+//            findViewById<Button>(R.id.cardFront)
+//        biometricLoginButton.setOnClickListener {
+//            biometricPrompt.authenticate(promptInfo)
+//        }
+
+
+    }
 
 
     private fun checkforUpdate() {
